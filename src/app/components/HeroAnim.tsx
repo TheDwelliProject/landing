5d762977d899efba525db2a1b72ee9fed1ad3d8e
Gabriel Esu <gabrielesu@Gabriels-MacBook-Pro-2.local>
2025-02-04 21:51:44 -0500

    Added interactive Hero rive animation

---
+++ src/app/components/HeroAnim.tsx
@@ -0,0 +1,17 @@
+import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
+
+export default function HeroAnim() {
+  const { RiveComponent } = useRive({
+    src:"/dwelli_flower.riv",
+    stateMachines: "State Machine 1",
+    layout: new Layout({
+      fit: Fit.Contain, // Change to: rive.Fit.Contain, or Cover
+      alignment: Alignment.Center,
+    }),
+    autoplay: true,
+  });
+
+  return (
+    <RiveComponent className='w-full sm:w-3/4 h-full absolute sm:-bottom-24 -bottom-48 -z-0' />
+  );
+}
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,9 +1,13 @@
+"use client";
+import HeroAnim from "./HeroAnim";
 import LinkButton from "./LinkButton";
+
 
 export default function HeroSection() {
   return (
-    <div className="flex w-full flex-col items-center pt-16 pb-36 px-8 bg-[url(/images/hero_bg.svg)] bg-contain bg-no-repeat bg-[center_bottom_1rem]">
-      <div className="flex flex-col items-center gap-4 text-center sm:w-1/3">
+    <div className="flex w-full flex-col items-center pt-16 pb-36 px-8 bg-contain bg-no-repeat bg-[center_bottom_1rem] relative">
+      <HeroAnim />
+      <div className="flex flex-col items-center gap-4 text-center sm:w-1/3 z-10">
        
         <h1 className="text-6xl sm:text-7xl font-semibold tracking-tight text-zinc-800">
           Power up your homes
